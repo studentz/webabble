c3c6540244613c46048f3873b2aacec87271aeca
Pablo Ordonez <pablo.f.ordonez@gmail.com>
2012-08-26 01:21:37 -0400

    add index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once('./FirePHPCore/FirePHP.class.php');
+$firephp = FirePHP::getInstance(true);
+include "./CommonPHP/hd.php";
+
+ ?>
+		<link rel="stylesheet" type="text/css" href="./Style/home.css" />
+		<script type="text/javascript" src="./CommonJS/Sharedfx.js"></script>
+		<script type="text/javascript" src="./CommonJS/swfobject.js"></script>
+ <?php
+ include"./CommonPHP/topnav.php";
+ include "./CommonPHP/sidebar.php";
+ ?>
+				<div id="home">
+				</div>
+ <?php
+ include "./CommonPHP/ft.php";
+?>
